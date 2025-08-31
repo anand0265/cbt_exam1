2467a3de56fba8100fa9d426d930ad663401b267
anand0265 <156602937+anand0265@users.noreply.github.com>
2025-08-31 15:39:44 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -60,14 +60,14 @@ app.use("/api/snapshot", snapshots);
 app.use("/api/audio", audio);
 
 app.use("/api/groups", groups);
-//createadmin();
+createadmin();
 
-if (process.env.NODE_ENV == "production") {
-  app.use(express.static("client/build"));
-  const path = require("path");
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-  });
-}
+// if (process.env.NODE_ENV == "production") {
+//   app.use(express.static("client/build"));
+//   const path = require("path");
+//   app.get("*", (req, res) => {
+//     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+//   });
+// }
 
 app.listen(PORT, () => console.log(`Listening on port ${PORT}...`));
