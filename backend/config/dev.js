42bf1a87afe33638431650d3ec7ebbcf53dc7eff
anand0265 <156602937+anand0265@users.noreply.github.com>
2025-08-31 14:45:25 +0530

    Update dev.js

--- backend/config/dev.js
+++ backend/config/dev.js
@@ -1,5 +1,5 @@
 module.exports = {
-  MONGO_URI: "mongodb+srv://anandp19900:nJ8biFEnRziaefix@cluster0.4c2dj.mongodb.net/cbt_exam",
+  MONGO_URI:"mongodb+srv://anandp19900:nJ8biFEnRziaefix@cluster0.4c2dj.mongodb.net/cbt_exam?retryWrites=true&w=majority",
   jwtPrivateKey: "dshksahdfkjashdkahjs",
   SENDGRID_API_KEY:
     "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",
